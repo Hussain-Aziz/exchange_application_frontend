ca220c3e0c0b001aac215c26a7ff110ad969acf7
Hussain Aziz <hussain.azizsaif@gmail.com>
2024-04-28 12:37:45 +0400

    fix incorrect endpoint for withdraw application

--- src/app/student/view_courses/page.tsx
+++ src/app/student/view_courses/page.tsx
@@ -1,4 +1,4 @@
-import { getHeaders, listCoursesEndpoint } from "../../../constants/endpoints";
+import { getHeaders, listCoursesEndpoint, submitApplicationEndpoint } from "../../../constants/endpoints";
 import { cookies } from 'next/headers';
 import CoursesContent from './content';
 import { revalidatePath } from 'next/cache';
@@ -14,12 +14,13 @@ export default async function Page() {
 
   const cancelApplication = async (id: number) => {
     "use server"
-    const response = await fetch(listCoursesEndpoint, {
+    const response = await fetch(submitApplicationEndpoint, {
       method: 'DELETE',
       headers: getHeaders(cookies()),
       body: JSON.stringify({ 'id': id })
     })
-    revalidatePath('/student/view_courses')
+    revalidatePath('/student/home/')
+    revalidatePath('/student/view_courses/')
   }
 
   return (
