b0f7dcbb6df30b0f96108a4af5a2ef57ac89b293
Hussain Aziz <hussain.azizsaif@gmail.com>
2024-05-04 00:46:13 +0400

    fix tests

--- src/app/faculty/__tests__/layout.test.tsx
+++ src/app/faculty/__tests__/layout.test.tsx
@@ -1,6 +1,20 @@
 import { render } from '@testing-library/react';
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import Layout from '../layout';
+
+vi.mock('next/navigation', () => ({
+  useRouter: () => ({
+    route: '/',
+    pathname: '',
+    query: '',
+    asPath: '',
+    push: vi.fn(),
+  }),
+  useSearchParams: () => ({
+    get: vi.fn(),
+  }),
+}));
+
 
 describe('Layout', () => {
   it('renders without crashing', () => {
